125f4aeae649726147f0d1f261315d0b0aadf92c
HerbertHe <Herbert.He0229@gmail.com>
2024-01-10 00:08:50 +0800

    fix :bug: : fix

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import { clone_supervisor_repo } from "./scripts/git.js"
 import { run_adapter } from "./scripts/adapter.js"
 
 const run = async () => {
-    clone_supervisor_repo().then(() => {
-        console.log("Run adapter...")
-        run_adapter()
-    })
+    console.log("Run adapter...")
+    run_adapter()
 }
 
 run()
