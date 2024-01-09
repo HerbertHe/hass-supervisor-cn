b70a1ef6c60c7aca9ade7400b86577cefa674f29
HerbertHe <Herbert.He0229@gmail.com>
2024-01-10 01:05:06 +0800

    fix :bug: : fix

--- scripts/consts.js
+++ scripts/consts.js
@@ -3,6 +3,6 @@ import path from "path"
 export const SUPERVISOR_REPO_URL =
     "git@github.com:home-assistant/supervisor.git"
 
-export const RAW_SOURCE_PATH = path.resolve()
+export const RAW_SOURCE_PATH = path.resolve("raw_source")
 
 export const SUPERVISOR_PATH = path.join(RAW_SOURCE_PATH, "supervisor")
